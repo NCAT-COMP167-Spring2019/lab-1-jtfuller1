b78584e1feef5c053d8f05ecb7c9956ef440be67
Jordan Lewis <jordanlewis@MacBook-Pro.local>
2018-01-10 13:08:07 -0500

    Lab-1

--- Contacts/src/contacts/Contact.java
+++ Contacts/src/contacts/Contact.java
@@ -23,6 +23,62 @@ public class Contact {
     }
     
     public String toString(){
-        return firstName + ", " + lastName + ", " + phoneNumber + ", " + emailAddress;
+        return getFirstName() + ", " + getLastName() + ", " + getPhoneNumber() + ", " + getEmailAddress();
+    }
+
+    /**
+     * @return the firstName
+     */
+    public String getFirstName() {
+        return firstName;
+    }
+
+    /**
+     * @param firstName the firstName to set
+     */
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    /**
+     * @return the lastName
+     */
+    public String getLastName() {
+        return lastName;
+    }
+
+    /**
+     * @param lastName the lastName to set
+     */
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    /**
+     * @return the phoneNumber
+     */
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    /**
+     * @param phoneNumber the phoneNumber to set
+     */
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    /**
+     * @return the emailAddress
+     */
+    public String getEmailAddress() {
+        return emailAddress;
+    }
+
+    /**
+     * @param emailAddress the emailAddress to set
+     */
+    public void setEmailAddress(String emailAddress) {
+        this.emailAddress = emailAddress;
     }
 }
